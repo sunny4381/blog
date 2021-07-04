bfe9e4f66faa3b4a02fd72748a388d2e7e738dc4
NAKANO Hideo <pinarello.marvel@gmail.com>
2021-07-04 17:57:30 +0900

    adds brand_logo content type validations

--- app/models/sys/tenant.rb
+++ app/models/sys/tenant.rb
@@ -8,8 +8,26 @@ class Sys::Tenant < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { maximum: 40 }
   validates :virtual_hosts, presence: true, length: { maximum: 10 }
   validates :brand_name, length: { maximum: 40 }
+  validate :validate_brand_logo
 
   after_save if: ->{ in_rm_brand_logo.present? } do |record|
-    record.brand_logo.purge if record.brand_logo.present?
+    record.brand_logo.purge if record.brand_logo.present? && record.brand_logo_blob.id == in_rm_brand_logo.to_s.to_i
+  end
+
+  private
+
+  def validate_brand_logo
+    return if brand_logo.blank? || !brand_logo.attached?
+
+    content_type = brand_logo.blob.try(:content_type)
+    if content_type.blank?
+      errors.add :brand_logo, :invalid_type
+      return
+    end
+
+    unless %w(image/png image/jpeg).include?(content_type)
+      errors.add :brand_logo, :invalid_type
+      return
+    end
   end
 end
