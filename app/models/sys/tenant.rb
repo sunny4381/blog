f74d4b13b09eab2eaaa128c674ca4c6730e38f01
NAKANO Hideo <pinarello.marvel@gmail.com>
2021-07-04 16:59:34 +0900

    adds active storage capability to support file attachments

--- app/models/sys/tenant.rb
+++ app/models/sys/tenant.rb
@@ -2,7 +2,14 @@ class Sys::Tenant < ApplicationRecord
   self.implicit_order_column = "created_at"
 
   has_many :virtual_hosts, as: :parent
+  has_one_attached :brand_logo
+  attr_accessor :in_rm_brand_logo
 
   validates :name, presence: true, uniqueness: true, length: { maximum: 40 }
-  validates :virtual_hosts, presence: true, length: { minimum: 10 }
+  validates :virtual_hosts, presence: true, length: { maximum: 10 }
+  validates :brand_name, length: { maximum: 40 }
+
+  after_save if: ->{ in_rm_brand_logo.present? } do |record|
+    record.brand_logo.purge if record.brand_logo.present?
+  end
 end
