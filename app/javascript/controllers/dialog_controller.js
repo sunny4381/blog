4527a111b1ba91c1193ca6fbb74583396a3b8ba7
NAKANO Hideo <pinarello.marvel@gmail.com>
2021-08-12 10:34:44 +0900

    adds dialog implementations with stimulus

--- app/javascript/controllers/dialog_controller.js
+++ app/javascript/controllers/dialog_controller.js
@@ -2,7 +2,30 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   open(ev) {
-    ev.preventDefault();
-    return false;
+    const href = ev.target.href
+    if (! href) {
+      return
+    }
+
+    fetch(href)
+      .then(response => response.text())
+      .then(html => this.openDialog(html))
+      .catch(err => this.showError(err))
+
+    const dropDown = ev.target.closest("[uk-dropdown]")
+    if (dropDown) {
+      UIkit.dropdown(dropDown).hide(false);
+    }
+
+    ev.preventDefault()
+    return false
+  }
+
+  openDialog(html) {
+    UIkit.modal.dialog(html)
+  }
+
+  showError(err) {
+    UIkit.modal.alert("Error")
   }
 }
