de6d341f40c7cbd54b944d5174b15a5af791f3a2
NAKANO Hideo <pinarello.marvel@gmail.com>
2021-08-12 14:30:16 +0900

    integrates "@hotwired/turbo-rails"

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,5 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
+
+import "@hotwired/turbo-rails"
