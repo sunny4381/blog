2388214a4dc1e2cb1df460a0ea85636a7ca946bb
NAKANO Hideo <pinarello.marvel@gmail.com>
2021-08-29 13:51:57 +0900

    refactors with RuboCop

--- db/migrate/20210822073854_add_id_to_group_closures.rb
+++ db/migrate/20210822073854_add_id_to_group_closures.rb
@@ -1,9 +1,13 @@
 class AddIdToGroupClosures < ActiveRecord::Migration[6.1]
   def change
     add_column :sys_group_closures, :id, :uuid, default: "gen_random_uuid()", null: false
-    execute "ALTER TABLE sys_group_closures ADD PRIMARY KEY (id);"
+    up_only do
+      execute "ALTER TABLE sys_group_closures ADD PRIMARY KEY (id);"
+    end
 
     add_column :sys_group_users, :id, :uuid, default: "gen_random_uuid()", null: false
-    execute "ALTER TABLE sys_group_users ADD PRIMARY KEY (id);"
+    up_only do
+      execute "ALTER TABLE sys_group_users ADD PRIMARY KEY (id);"
+    end
   end
 end
